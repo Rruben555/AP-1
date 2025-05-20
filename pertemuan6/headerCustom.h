b027f9392b9327f6cac3e5b2b4de287e32beb2f7
Rruben555 <rubensiregar25@gmail.com>
2025-05-20 18:40:42 +0700

    Update headerCustom.h

--- pertemuan6/headerCustom.h
+++ pertemuan6/headerCustom.h
@@ -1,8 +1,8 @@
-#define u using namespace std;
-#define L <<
-#define o cout
-#define e endl
-#define begin int main
-#define s system("CLS")
-#define r return
-#define i int
+#define u using namespace std; //mengganti using namespace std dengan u
+#define L << // mengganti << dengan L
+#define o cout // mengganti cout dengan o
+#define e endl //mengganti endl dengan e
+#define begin int main // mengganti int main dengan begin
+#define s system("CLS") // mengganti system("CLS") dengan s
+#define r return //mengganti return dengan r
+#define i int //mengganti int dengan i
