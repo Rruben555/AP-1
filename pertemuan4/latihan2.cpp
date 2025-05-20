4e5a0a144a69af6dd0896a9cb8b5cd9bee507dfd
Rruben555 <rubensiregar25@gmail.com>
2025-05-20 11:04:12 +0700

    Update latihan2.cpp

--- pertemuan4/latihan2.cpp
+++ pertemuan4/latihan2.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main(){
-    string kalimat;
+    string kalimat;//deklarasi variabel
     int i;
 
     cout<<"Masukkan kalimat: ";
-    getline(cin, kalimat);
+    getline(cin, kalimat);// input kalimat (spasi dibaca)
     
-    for(i=0; i<kalimat.length(); i++){
-        if (kalimat[i]=='a' || kalimat[i]=='i' ||kalimat[i]=='u' ||kalimat[i]=='e' || kalimat[i]=='o'){
+    for(i=0; i<kalimat.length(); i++){// jika i<kalimat.length() masih terpenuhi akan dijalankan
+        if (kalimat[i]=='a' || kalimat[i]=='i' ||kalimat[i]=='u' ||kalimat[i]=='e' || kalimat[i]=='o'){// jika terdapat char di kalimat yang berupa a,i,u,e,o
 
-        kalimat[i]=toupper(kalimat[i]);
+        kalimat[i]=toupper(kalimat[i]);//akan di kaptialkan
         }
 
     }
 
-    cout<<"Kalimat dalam huruf kapital: "<<kalimat<<endl;
+    cout<<"Hasil: "<<kalimat<<endl;//tampilkan kalimat yang sudah dimodifikasi
 }
