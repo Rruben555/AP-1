fc9e052f88107d9b477929eccfcc2008328459f0
Rruben555 <rubensiregar25@gmail.com>
2025-05-20 17:00:28 +0700

    Update belajarFungsi.cpp

--- pertemuan6/belajarFungsi.cpp
+++ pertemuan6/belajarFungsi.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama){
+void sapa(string nama){//inisialisasi fungsi sapa, void karena tidak mengembalikan nilai, nama sebagai parameter formal
     cout<<"Halo "<<nama<<"! Selamat belajar C++!"<<endl;
 }
 
 int main(){
     string namaPengguna="Ruben";
-    sapa(namaPengguna);
+    sapa(namaPengguna);//pemanggilan fungsi sapa dengan namaPengguna sebagai parameter aktual
 
     return 0;
 }
