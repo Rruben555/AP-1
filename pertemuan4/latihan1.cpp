14f938fbab453cfa43856b84a08dd44ee198949b
Rruben555 <rubensiregar25@gmail.com>
2025-05-20 10:59:26 +0700

    Update latihan1.cpp

--- pertemuan4/latihan1.cpp
+++ pertemuan4/latihan1.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-    string kalimat;
+    string kalimat;//deklarasi variabel
     int i;
 
     cout<<"Masukkan kalimat: ";
-    getline(cin, kalimat);
+    getline(cin, kalimat);//input kalimat (spasi tetap dibaca)
     
-    for(i=0; i<kalimat.length(); i++){
-        kalimat[i]=toupper(kalimat[i]);
+    for(i=0; i<kalimat.length(); i++){// sampai i=kalimat.length() akan dijalankan
+        kalimat[i]=toupper(kalimat[i]);//semua char di kalimat akan di kapitalkan
 
     }
 
-    cout<<"Kalimat dalam huruf kapital: "<<kalimat<<endl;
+    cout<<"Kalimat dalam huruf kapital: "<<kalimat<<endl;//tampilkan kalimat yang sepenuhnya dalam kapital
 }
