2359a34e03507eba562d8dc47afecdf5a4b4cd79
Rruben555 <rubensiregar25@gmail.com>
2025-05-20 10:19:48 +0700

    Update latihan.cpp

--- pertemuan2/latihan.cpp
+++ pertemuan2/latihan.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-    float volume, lp;
+    float volume, lp; // deklarast variabel
     int r;
-    const float pi =3.14;
+    const float pi =3.14; // deklarasi konstanta
     cout<<"Masukkan radius : ";
-    cin>>r;
+    cin>>r; // input r
 
-    volume=(float)4/(float)3*pi*r*r*r;
-    lp=4*pi*r*r;
+    volume=(float)4/(float)3*pi*r*r*r;// hitung volume
+    lp=4*pi*r*r; //hitung lp
 
-    cout<<volume<<endl;
-    cout<<lp<<endl;
+    cout<<volume<<endl; //tampilkan volume
+    cout<<lp<<endl; //tampilkan lp
 
 }
