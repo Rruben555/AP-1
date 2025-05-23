bb91c650c57ce8d66dd4d7a35be0a1016e8fc176
Rruben555 <rubensiregar25@gmail.com>
2025-05-23 09:27:45 +0700

    Update struct.cpp

--- pertemuan8/struct.cpp
+++ pertemuan8/struct.cpp
@@ -3,12 +3,12 @@
 #include<vector>
 using namespace std;
 
-struct Alamat{
+struct Alamat{ //deklarasi struct
     string jalan,kota;
     int kodePos;
 };
 
-struct Mahasiswa{
+struct Mahasiswa{ //deklarasi struct
     string nama;
     int umur;
     float ipk;
@@ -17,19 +17,19 @@ struct Mahasiswa{
 
 int main(){
     system("CLS");
-    Mahasiswa mhs1;
+    Mahasiswa mhs1; //variabel dengan bentuk Mahasiswa
     // mhs1.alamat.jalan="Jalan Jamin Ginting";
     // mhs1.alamat.kota="Medan";
     // mhs1.alamat.kodePos=20155;
 
     // cout<<"Alamat: "<<mhs1.alamat.jalan<<" "<<mhs1.alamat.kota<<" "<<mhs1.alamat.kodePos<<endl;
 
-    vector<Mahasiswa> mahasiswa;
+    vector<Mahasiswa> mahasiswa; //vector struct Mahasiswa
     int n;
     cout<<"Masukkan banyak mahasiswa: ";
     cin>>n;
 
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n; i++){ //input ke mhs1
         cout<<"Mahasiswa "<<i+1<<endl;
         cin.get();
         cout<<"Masukkan nama: ";
@@ -38,10 +38,10 @@ int main(){
         cin>>mhs1.umur;
         cout<<"Masukkan ipk: ";
         cin>>mhs1.ipk;
-        mahasiswa.push_back(mhs1);
+        mahasiswa.push_back(mhs1); //masukkan data dari mhs1 ke vector mahasiswa
     }
 
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n; i++){ //output dari mahasiswa
         cout<<"Mahasiswa "<<i+1<<endl;
         cout<<"Nama: "<<mahasiswa[i].nama<<endl;
         cout<<"Umur: "<<mahasiswa[i].umur<<endl;
