53e8cb4351269bb3b9293cfa7df73ac2093394af
Rruben555 <rubensiregar25@gmail.com>
2025-05-23 09:22:09 +0700

    Update aksesStruct.cpp

--- pertemuan8/aksesStruct.cpp
+++ pertemuan8/aksesStruct.cpp
@@ -14,12 +14,12 @@ int main(){
     mhs1.umur=18;
     mhs1.ipk=3.8;
 
-    // cout<<"Akses dengan . : "<<endl;
+    // cout<<"Akses dengan . : "<<endl; //dapat diakses dengan .
     // cout<<"Nama: "<<mhs1.nama<<endl;
     // cout<<"Umur: "<<mhs1.umur<<endl;
     // cout<<"IPK: "<<mhs1.ipk<<endl;
 
-    Mahasiswa *ptrMhs=&mhs1;
+    Mahasiswa *ptrMhs=&mhs1; // dapat diakses dengan ->
     cout<<"Akses dengan -> : "<<endl;
     cout<<"Nama: "<<ptrMhs->nama<<endl;
     cout<<"Umur: "<<ptrMhs->umur<<endl;
