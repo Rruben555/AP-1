c2a35362fbc49071807bb40aa9dd0b0879f25503
Rruben555 <rubensiregar25@gmail.com>
2025-05-23 12:05:03 +0700

    Update pointer.cpp

--- pertemuan7/pointer.cpp
+++ pertemuan7/pointer.cpp
@@ -6,7 +6,7 @@ void penjumlahan(int a,int b){
 }
 
 void penjumlahan_pointer(int *a,int *b){
-    *a+=*b;
+    *a+=*b;//nilai yang ditunjuk pointer a diubah
     cout<<*a<<endl;
 }
 
@@ -23,33 +23,33 @@ int main(){
     int *pointer_number=&number;
 
     // cout<<"Isi variabel number= "<<number<<endl;
-    // cout<<"Alamat memori variabel number= "<<&number<<endl;
-    // cout<<"Isi variabel pointer_number= "<<pointer_number<<endl;
-    // cout<<"Isi variabel yang ditunjuk oleh pointer_number= "<<*pointer_number<<endl;
-    // cout<<"Alamat memori pointer_number= "<<&pointer_number<<endl;
+    // cout<<"Alamat memori variabel number= "<<&number<<endl; //menampilkan alamat variabel
+    // cout<<"Isi variabel pointer_number= "<<pointer_number<<endl; //menampilkan alamat variabel
+    // cout<<"Isi variabel yang ditunjuk oleh pointer_number= "<<*pointer_number<<endl; //menampilkan isi variabel yang di tunjuk pointer
+    // cout<<"Alamat memori pointer_number= "<<&pointer_number<<endl; //menampilkan alamat pointer
     // cout<<endl;
 
     //Pointer Operation
-    // *pointer_number=25;
-    // cout<<"Isi variabel number= "<<number<<endl;
-    // cout<<"Alamat memori variabel number= "<<&number<<endl;
-    // cout<<"Isi variabel pointer_number= "<<pointer_number<<endl;
-    // cout<<"Isi variabel yang ditunjuk oleh pointer_number= "<<*pointer_number<<endl;
-    // cout<<"Alamat memori pointer_number= "<<&pointer_number<<endl;
+    // *pointer_number=25; //mengganti isi variabel
+    // cout<<"Isi variabel number= "<<number<<endl; //isi variabel diganti
+    // cout<<"Alamat memori variabel number= "<<&number<<endl; //alamat tetap
+    // cout<<"Isi variabel pointer_number= "<<pointer_number<<endl; //alamat tetap
+    // cout<<"Isi variabel yang ditunjuk oleh pointer_number= "<<*pointer_number<<endl; //isi variabel diganti
+    // cout<<"Alamat memori pointer_number= "<<&pointer_number<<endl; //alamat tetap
     // cout<<endl;
 
     //Pointer in Array
     int num[]={1,2,3,4,5};
     int *pointer_num=num;
-    // cout<<"Isi variabel num= "<<num[0]<<endl;
-    // cout<<"Alamat memori variabel num= "<<&num[3]<<endl;
-    // cout<<"Isi variabel pointer_num= "<<pointer_num<<endl;
-    // cout<<"Isi variabel yang ditunjuk oleh pointer_num= "<<*pointer_num<<endl;
-    // cout<<"Alamat memori pointer_num= "<<&pointer_num<<endl;
+    // cout<<"Isi variabel num= "<<num[0]<<endl; 
+    // cout<<"Alamat memori variabel num= "<<&num[3]<<endl; //alamat array indeks ke 3
+    // cout<<"Isi variabel pointer_num= "<<pointer_num<<endl; //menampilkan alamat indeks ke 0
+    // cout<<"Isi variabel yang ditunjuk oleh pointer_num= "<<*pointer_num<<endl; //isi indeks ke 0
+    // cout<<"Alamat memori pointer_num= "<<&pointer_num<<endl; //alamat pointer
     // cout<<endl;
 
-    // *pointer_num+=5;
-    // cout<<"Isi variabel num= "<<num[0]<<endl;
+    // *pointer_num+=5; //nilai pointer diganti
+    // cout<<"Isi variabel num= "<<num[0]<<endl; //nilai indeks ke 0 berubah, alamat tetap
     // cout<<"Alamat memori variabel num= "<<&num[3]<<endl;
     // cout<<"Isi variabel pointer_num= "<<pointer_num<<endl;
     // cout<<"Isi variabel yang ditunjuk oleh pointer_num= "<<*pointer_num<<endl;
@@ -63,7 +63,7 @@ int main(){
     // cout<<num1<<endl;
     // cout<<num2<<endl;
     // penjumlahan_pointer(&num1,&num2);
-    // cout<<num1<<endl;
+    // cout<<num1<<endl; //isi variabel diubah
     // cout<<num2<<endl;
 
     //Pointer in Pointer
@@ -73,14 +73,14 @@ int main(){
     // cout<<"Isi variabel score= "<<score<<" dan alamat memorinya "<<&score<<endl;
     // cout<<"Isi variabel pointer_score "<<pointer_score<<" dan isi variabel yang ditunjuk oleh pointer score "<<*pointer_score<<" dan alamat memori pointer_score "<<&pointer_score<<endl;
     // cout<<"Isi variabel ptr_pointer_score "<<ptr_pointer_score<<" dan isi variabel yang ditunjuk oleh ptr_pointer_score "<<**ptr_pointer_score<<" dan alamat memori ptr_pointer_score "<<&ptr_pointer_score<<endl;
-
-    //Dynamic Pointer
+    //isi ptr_pointer_score=alamat pointer_score, isi variabel yg ditunjuk=isi score
+    //Dynamic Pointer, membuat pointer baru tanpa variabel lain
     // int *ptr=new int;
     // *ptr=30;
     // cout<<"Isi variabel ptr "<<*ptr<<" dan alamat ptr "<<&ptr<<endl;
     
     // delete ptr;//deallocation
-    // cout<<"Isi variabel ptr "<<*ptr<<" dan alamat ptr "<<&ptr<<endl;
+    // cout<<"Isi variabel ptr "<<*ptr<<" dan alamat ptr "<<&ptr<<endl; //*ptr berupa angka random, alamay tetap sama
 
     //Contoh
     // int n;
